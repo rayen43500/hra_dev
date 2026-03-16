a7dc73e13c0fa0d6b069e78fa46bf5eec2ebaa33
rayen43500 <rayen.9b7@gmail.com>
2026-03-16 15:53:29 +0100

    git h p

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 }) 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+// Remplacez 'nom-du-repo' par le nom de votre repository GitHub
+export default defineConfig({
+  base: '/hra_dev/',
+  plugins: [react()],
+})
